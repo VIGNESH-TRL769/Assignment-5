46a08af02f94fffe1e2a1623360555b550afa7f7
VIGNESH-TRL769 <123439677+VIGNESH-TRL769@users.noreply.github.com>
2023-01-30 18:06:38 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,11 @@ class Coordinates implements Cloneable
     int x;
     int y;
 
-     Coordinates(int x,int y)
-     {
-         this.x=x;
-         this.y=y;
-     }
+    Coordinates(int x,int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
     public Object clone() throws CloneNotSupportedException
     {
         return super.clone();
@@ -19,10 +19,10 @@ class Coordinates implements Cloneable
 class Check
 {
 
-   boolean check(int x,int y,int x1,int y1)
-   {
-       return x==x1 && y==y1;
-   }
+    boolean check(int x,int y,int x1,int y1)
+    {
+        return x==x1 && y==y1;
+    }
 
 }
 public class Main
@@ -30,25 +30,35 @@ public class Main
     public static final Logger LOGGER =  Logger.getLogger("InfoLogging");
     public static void main(String[] args)  throws CloneNotSupportedException
     {
-        Scanner input=new Scanner(System.in);
-        LOGGER.info("Enetr the X value:");
-        int x=input.nextInt();
-        LOGGER.info("Enter thr Y value:");
-        int y=input.nextInt();
-        LOGGER.info("Enetr the X1 value:");
-        int x1=input.nextInt();
-        LOGGER.info("Enter thr Y1 value:");
-        int y1=input.nextInt();
-        Coordinates coordinates=new Coordinates(x,y);
-        Check check=new Check();
-        String checkValue=String.valueOf(check.check(x,y,x1,y1));
-        LOGGER.info(checkValue);
-        Coordinates cloneObject=(Coordinates)coordinates.clone();
-        cloneObject.x=x1;
-        cloneObject.y=y1;
-        String original="Orignial Coordinates of X and Y  is X:"+coordinates.x+" Y:"+coordinates.y;
-        String cloneValue="Cloned Coordinates of X and Y  is X:"+cloneObject.x+" Y:"+cloneObject.y;
-        LOGGER.info(original);
-        LOGGER.info(cloneValue);
+        Scanner input = new Scanner(System.in);
+        try 
+        {
+            LOGGER.info("Enetr the X value:");
+            int x = input.nextInt();
+            LOGGER.info("Enter thr Y value:");
+            int y = input.nextInt();
+            LOGGER.info("Enetr the X1 value:");
+            int x1 = input.nextInt();
+            LOGGER.info("Enter thr Y1 value:");
+            int y1 = input.nextInt();
+            Coordinates coordinates = new Coordinates(x, y);
+            Check check = new Check();
+            String checkValue = String.valueOf(check.check(x, y, x1, y1));
+            LOGGER.info(checkValue);
+            Coordinates cloneObject = (Coordinates) coordinates.clone();
+            cloneObject.x = x1;
+            cloneObject.y = y1;
+            String original = "Orignial Coordinates of X and Y  is X:" + coordinates.x + " Y:" + coordinates.y;
+            String cloneValue = "Cloned Coordinates of X and Y  is X:" + cloneObject.x + " Y:" + cloneObject.y;
+            LOGGER.info(original);
+            LOGGER.info(cloneValue);
+        } catch(Exception e)
+        {
+            String value = "Please Enter Valid Numbers:" + e;
+            LOGGER.info(value);
+            input.nextLine();
+        }
+
+
     }
 }
